493f096c3bf2571308a5f1d8ad785e31abb61b8b
iSuyash69 <suyashdeshpande479@gmail.com>
2024-10-12 08:57:35 +0530

    rotate device for tablets functionality added

--- app/components/LoadingPage/LoadingPage.jsx
+++ app/components/LoadingPage/LoadingPage.jsx
@@ -1,16 +1,30 @@
 import { ThemeContext } from "@/app/context/themeContext";
-import { useContext } from "react";
+import { useContext, useEffect, useState } from "react";
 import { BlinkBlur, Mosaic } from "react-loading-indicators";
 
 const LoadingPage=()=>{
 
     const { darkMode } = useContext(ThemeContext);
-    
+    const [screenWidth, setScreenWidth] = useState(window.innerWidth);
+
+    useEffect(() => {
+        const handleResize = () => {
+            setScreenWidth(window.innerWidth); 
+        };
+
+        window.addEventListener("resize", handleResize); 
+
+        return () => {
+            window.removeEventListener("resize", handleResize);
+        };
+    }, []);
+
     const mosaicColor = darkMode ? "white" : "black";
+    const mosaicSize = screenWidth < 768 ? "small" : "large";
 
     return(
         <div className={`flex justify-center items-center h-screen ${darkMode ? 'bg-darker' : 'bg-white'}`}>
-            <Mosaic color={mosaicColor} speedPlus="0" size="large"/>
+            <Mosaic color={mosaicColor} speedPlus="0"  size={mosaicSize}/>
         </div>
     );
 }
--- app/page.js
+++ app/page.js
@@ -9,11 +9,15 @@ import { ThemeContext } from "@/app/context/themeContext";
 import LoadingPage from "@/app/components/LoadingPage/LoadingPage";
 import ContactPage from "@/app/components/ContactPage/ContactPage";
 import MilestonesPage from "./components/MilestonesPage/MilestonesPage";
+import rotateTabletImg from "../app/assets/images/rotateTabletImg.png";
+import Image from "next/image";
 
 const Page = () => {
 
   const { darkMode } = useContext(ThemeContext);
   const [loading, setLoading] = useState(true);
+  const [showRotateMessage, setShowRotateMessage] = useState(false);
+  const [isMobileDevice,setIsMobileDevice] = useState(false);
 
   useEffect(() => {
 
@@ -30,14 +34,45 @@ const Page = () => {
             window.addEventListener("load", handleTimeout);
         }
 
+        const handleOrientationChange = () => {
+            const width = window.innerWidth;
+            const height = window.innerHeight;
+      
+            // Showing the rotate message only for tablets in portrait mode (width between 768px and 1024px)
+            if (height > width && width >= 768 && width <= 1024) {
+              setShowRotateMessage(true);
+            } else {
+              setShowRotateMessage(false);
+            }
+
+            if (width < 768){
+                setIsMobileDevice(true);
+            }
+            else{
+                setIsMobileDevice(false);
+            }
+          };
+      
+          // Initial check
+          handleOrientationChange();
+      
+          // event listener for orientation change
+          window.addEventListener("resize", handleOrientationChange);
+      
+          return () => {
+            window.removeEventListener("resize", handleOrientationChange);
+          };
+
     }, []);
+
+    console.log(showRotateMessage);
     
     if (loading) {
         return <LoadingPage/>; // Showing loading screen until the delay finishes
     }
 
     return (
-        <div className={`flex flex-col  transition-all ease-in-out ${darkMode ? 'bg-darker' : 'bg-white'}`}>
+        <div className={`flex flex-col transition-all ease-in-out ${darkMode ? 'bg-darker' : 'bg-white'}`}>
             <NavBar/>
             <div style={{height: "calc(100vh - 65.6px)"}} className="overflow-x-auto no_scrollbar flex scroll-smooth snap-x snap-mandatory">
                 <section id="Home" className="min-w-full flex items-center justify-center" style={{ scrollSnapAlign: "center" }}>
@@ -57,6 +92,19 @@ const Page = () => {
                 </section>
                 <CustomCursor/>
             </div>
+
+            <div className={`fixed top-0 left-0 w-full h-full flex flex-col justify-center items-center text-2xl z-50 pointer-events-none transition-all duration-300 ease-in-out bg-opacity-85 backdrop-blur-lg opacity-0 ${darkMode ? 'bg-darker text-white' : 'bg-white text-black'} ${showRotateMessage ? "rotate-device" : ""}`}>
+                <div className="flex flex-col justify-center mb-32 items-center">
+                    <Image className={`${darkMode ? '' : 'invert'}`} src={rotateTabletImg} alt="img"/>
+                    <p>Please rotate your device to landscape mode</p>
+                </div>
+            </div>
+            
+            <div className={`absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 w-full h-full flex flex-col justify-center items-center text-lg z-50 pointer-events-none transition-all duration-300 ease-in-out bg-opacity-85 backdrop-blur-lg opacity-0 ${darkMode ? 'bg-darker text-white' : 'bg-white text-black'} ${isMobileDevice ? "" : ""}`}>
+                <p>Please open in laptop/desktop</p>
+                <p>Mobile version coming soon!</p>
+            </div>
+
         </div>
     );
 };
